057c3c34ba1c1b8721d07baabe1a98a1d7ecf98f
Nikita Mitasov <me@ch4og.com>
2026-07-20 12:53:59 +0300

    packages: mangowm: init at 0.15.4

---
+++ shika/packages/mangowm.scm
@@ -0,0 +1,98 @@
+;;; SPDX-FileCopyrightText: 2025-2026 Nikita Mitasov <me@ch4og.com>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (shika packages mangowm)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (gnu packages window-management)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages pciutils)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages pcre)
+  #:use-module (gnu packages javascript)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages build-tools)
+  #:use-module (gnu packages ninja)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (guix build-system meson)
+  #:use-module ((guix licenses) #:prefix license:))
+
+
+(define-public mangowm
+  (package
+    (name "mangowm")
+    (version "0.15.4")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/mangowm/mango")
+              (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "07xa4jc51rhbjjp0qjgbwwqgg02399x02b8fngxpss9aq5z7r6bz"))))
+    (build-system meson-build-system)
+    (arguments
+     (list
+      #:configure-flags
+      #~(list (string-append "-Dsysconfdir=" #$output "/etc"))
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'configure 'patch-meson
+            (lambda _
+              (substitute* "meson.build"
+                ;; MangoWM ignores sysconfdir handling for NixOS.
+                ;; We also need to skip that sysconfdir edits.
+                (("is_nixos = false")
+                 "is_nixos = true")
+                ;; Unhardcode path.  Fixes loading default config.
+                (("'-DSYSCONFDIR=\\\"@0@\\\"'.format\\('/etc'\\)")
+                 "'-DSYSCONFDIR=\"@0@\"'.format(sysconfdir)")))))))
+    (inputs (list wayland
+                  libinput
+                  libdrm
+                  libxkbcommon
+                  pixman
+                  libdisplay-info
+                  libliftoff
+                  hwdata
+                  seatd
+                  pcre2
+                  pango
+                  cjson
+                  libxcb
+                  pixman
+                  xcb-util-wm
+                  wlroots-0.20
+                  scenefx-0.5))
+    (native-inputs (list pkg-config wayland-protocols))
+    (home-page "https://github.com/mangowm/mango")
+    (synopsis "Wayland compositor based on wlroots and scenefx")
+    (description
+     "MangoWM is a modern, lightweight, high-performance Wayland compositor
+built on dwl — crafted for speed, flexibility, and a customizable desktop experience.")
+    (license (list license:gpl3    ;mangowm itself, dwl
+                   license:expat   ;dwm, sway, wlroots
+                   license:cc0)))) ;tinywl
+
+(define scenefx-0.5
+  (package
+    (inherit scenefx)
+    (name "scenefx")
+    (version "0.5")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/wlrfx/scenefx")
+                     (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0klxy73125lp9jab8qghh4v6di91l3y2rgan4m4lhv5flwdwnj5x"))))
+    (inputs (modify-inputs (package-inputs scenefx)
+              (replace "wlroots" wlroots-0.20)))))
