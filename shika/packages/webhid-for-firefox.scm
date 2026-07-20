eb9898f26aeecdbe037d2d1bb132f9c8eaf93ad1
Nikita Mitasov <me@ch4og.com>
2026-07-20 19:05:56 +0300

    packages: webhid-for-firefox: fix hash

--- shika/packages/webhid-for-firefox.scm
+++ shika/packages/webhid-for-firefox.scm
@@ -24,7 +24,7 @@
              (url "https://github.com/calamity-inc/Soup")
              (commit commit)))
        (sha256
-        (base32 "1ji78qz7xkm5jqmcpqfy0h8914w8v66yzhh7yiyg54vy71h2rw4q"))))
+        (base32 "1b7n2hhxnsd7gkl8k4mn5dz9qfyaxa9pqa8ri59xxbm30v0cfzvh"))))
     (build-system gnu-build-system)
     (arguments
      (list
@@ -123,7 +123,7 @@ not work with this forked server.")
                                    "/releases/download/" version
                                    "/WebHID-for-Firefox.xpi"))
                (sha256
-                (base32 "1z11r1111fi3hc3pixcffi0kz2650lc11ni8rlwfadlhz8vra57c"))))
+                (base32 "0zc1p5w1dddcmnrx6mdrw4av71y49wgsv04mzql3y12wlx6vjizs"))))
      (build-system copy-build-system)
      (arguments
       (list #:install-plan
