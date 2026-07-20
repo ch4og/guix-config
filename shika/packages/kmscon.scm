5cfb9f2f7dac8c72d2c9bc1453c624a14f50d8cc
Nikita Mitasov <me@ch4og.com>
2026-07-20 14:13:50 +0300

    packages: kmscon: 9.3.3->10.0.1

--- shika/packages/kmscon.scm
+++ shika/packages/kmscon.scm
@@ -7,16 +7,22 @@
   #:use-module (gnu packages terminals)
   #:use-module (shika utils override))
 
-(define-public libtsm
-  (shika-override (@ (gnu packages terminals) libtsm)
-                  #:version "4.4.3"
-                  #:hash "011zpzzfbhca22vfsdmp9lf6i99x7h5blp3vv0nl2pav029bnv0d"))
+(define libtsm-guix
+  (@ (gnu packages terminals) libtsm))
+
+(define kmscon-guix
+  (@ (gnu packages terminals) kmscon))
+
+(define libtsm-shika
+  (shika-override libtsm-guix
+                  #:version "4.6.0"
+                  #:hash "0lrmdaqlaq97slwi4n99m4lr3b71vzq0xck8i3kbnzdw92v69ph8"))
 
 (define-public kmscon
-  (shika-override (@ (gnu packages terminals) kmscon)
-                  #:version "9.3.3"
-                  #:hash "0b2hv8zbp20iqa6zgwvzdvahs5wg3axv8aw1xk81qs7rjsaw7n2k"
-                  #:inputs (modify-inputs (package-inputs (@ (gnu packages terminals) kmscon))
-                             (replace "libtsm" libtsm))))
+  (shika-override kmscon-guix
+                  #:version "10.0.1"
+                  #:hash "162nxqrlsvsdf6pypqdd3s54ac3c6vfnchfk8dmmy4nzlj5acm5y"
+                  #:inputs (modify-inputs (package-inputs kmscon-guix)
+                             (replace "libtsm" libtsm-shika))))
 
 kmscon
