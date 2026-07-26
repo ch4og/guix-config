9ed0a73b7bc2fa55fc8b1c11e1e2345f9a04d834
Nikita Mitasov <me@ch4og.com>
2026-07-26 05:29:40 +0300

    packages: opentabletdriver: add check phase and debug stripping

--- shika/packages/opentabletdriver.scm
+++ shika/packages/opentabletdriver.scm
@@ -37,7 +37,33 @@
         #:phases
         #~(modify-phases %standard-phases
             (delete 'configure)
-            (delete 'check)
+            (replace 'check
+              (lambda* (#:key inputs #:allow-other-keys)
+                (let ((dotnet-bin (string-append (assoc-ref inputs "dotnet") "/bin/dotnet"))
+                      (nuget-pkgs (string-append
+                                   (assoc-ref inputs "restored-nuget-dependencies")
+                                   "/nuget-packages")))
+                  (setenv "DOTNET_CLI_TELEMETRY_OPTOUT" "1")
+                  (setenv "DOTNET_NOLOGO" "1")
+                  (setenv "DOTNET_SKIP_FIRST_TIME_EXPERIENCE" "1")
+                  (setenv "NUGET_PACKAGES" nuget-pkgs)
+                  (let ((home (getenv "TMPDIR")))
+                    (setenv "HOME" home)
+                    (mkdir-p (string-append home "/.dotnet")))
+                  (invoke dotnet-bin "test"
+                          "OpenTabletDriver.Tests/OpenTabletDriver.Tests.csproj"
+                          "--framework" "net8.0"
+                          "--configuration" "Release"
+                          "--no-restore"
+                          "--filter" (string-append
+                                     "FullyQualifiedName!=OpenTabletDriver.Tests.UpdaterTests.CheckForUpdates_Returns_Update_When_Available"
+                                     "&FullyQualifiedName!=OpenTabletDriver.Tests.UpdaterTests.Install_Throws_UpdateAlreadyInstalledException_When_AlreadyInstalled"
+                                     "&FullyQualifiedName!=OpenTabletDriver.Tests.UpdaterTests.Install_DoesNotThrow_UpdateAlreadyInstalledException_When_PreviousInstallFailed"
+                                     "&FullyQualifiedName!=OpenTabletDriver.Tests.UpdaterTests.Install_Throws_UpdateInProgressException_When_AnotherUpdate_Is_InProgress"
+                                     "&FullyQualifiedName!=OpenTabletDriver.Tests.UpdaterTests.Install_Moves_UpdatedBinaries_To_BinDirectory"
+                                     "&FullyQualifiedName!=OpenTabletDriver.Tests.UpdaterTests.Install_Moves_Only_ToBeUpdated_Binaries"
+                                     "&FullyQualifiedName!=OpenTabletDriver.Tests.UpdaterTests.Install_Copies_AppDataFiles"
+                                     "&FullyQualifiedName!=OpenTabletDriver.Tests.TimerTests.TimerAccuracy")))))
             (replace 'build
               (lambda* (#:key inputs #:allow-other-keys)
                 (let ((dotnet-bin (string-append (assoc-ref inputs "dotnet") "/bin/dotnet"))
@@ -69,12 +95,13 @@
                   (for-each
                    (lambda (project)
                      (invoke dotnet-bin "publish" project
-                             "--framework" "net8.0"
-                             "--configuration" "Release"
-                             "--no-restore"
-                             "--output" "dist"
-                             "-p:PublishTrimmed=false"
-                             "-p:DebugType=embedded"))
+                           "--framework" "net8.0"
+                           "--configuration" "Release"
+                           "--no-restore"
+                           "--output" "dist"
+                           "-p:PublishTrimmed=false"
+                           "-p:DebugType=None"
+                           "-p:DebugSymbols=false"))
                    '("OpenTabletDriver.Daemon"
                      "OpenTabletDriver.Console"
                      "OpenTabletDriver.UX.Gtk")))))
@@ -83,6 +110,9 @@
                 (let ((out (assoc-ref outputs "out")))
                   (mkdir-p (string-append out "/bin"))
                   (copy-recursively "dist" (string-append out "/lib/opentabletdriver"))
+                  (when (file-exists? "OpenTabletDriver.Configurations/Configurations")
+                    (copy-recursively "OpenTabletDriver.Configurations/Configurations"
+                                      (string-append out "/lib/opentabletdriver/Configurations")))
                   (let ((lib-paths
                          (string-join
                           (let loop ((rest inputs) (result '()))
@@ -99,18 +129,20 @@
                      (lambda (pair)
                        (let ((name (car pair))
                              (dll (cdr pair)))
-                         (call-with-output-file (string-append out "/bin/" name)
-                           (lambda (port)
-                             (format port
-                                     "#!~a~%\
+                          (call-with-output-file (string-append out "/bin/" name)
+                            (lambda (port)
+                              (format port
+                                      "#!~a~%\
 export DOTNET_ROOT=~a~%\
 export LD_LIBRARY_PATH=\"~a:${LD_LIBRARY_PATH:-}\"~%\
+export OTD_CONFIGURATIONS=\"~a/lib/opentabletdriver/Configurations\"~%\
 exec ~a/bin/dotnet ~a/lib/opentabletdriver/~a \"$@\"~%"
-                                     #$(file-append bash-minimal "/bin/bash")
-                                     (assoc-ref inputs "dotnet")
-                                     lib-paths
-                                     (assoc-ref inputs "dotnet")
-                                     out dll)))
+                                      #$(file-append bash-minimal "/bin/bash")
+                                      (assoc-ref inputs "dotnet")
+                                      lib-paths
+                                      out
+                                      (assoc-ref inputs "dotnet")
+                                      out dll)))
                          (chmod (string-append out "/bin/" name) #o755)))
                      '(("otd-daemon" . "OpenTabletDriver.Daemon.dll")
                        ("otd" . "OpenTabletDriver.Console.dll")
@@ -136,6 +168,15 @@ StartupWMClass=OpenTabletDriver.UX
                         (mkdir-p (string-append out "/share/icons/hicolor/256x256/apps"))
                         (copy-file (string-append assets "/otd.png")
                                    (string-append out "/share/icons/hicolor/256x256/apps/otd.png"))))
+                    (mkdir-p (string-append out "/share/libinput"))
+                    (call-with-output-file
+                        (string-append out "/share/libinput/30-vendor-opentabletdriver.quirks")
+                      (lambda (port)
+                        (display "[OpenTabletDriver Virtual Tablets]
+MatchName=OpenTabletDriver*
+AttrTabletSmoothing=0
+AttrPressureRange=2:1
+" port)))
                     #t)))))))
      (native-inputs
       (list (origin
