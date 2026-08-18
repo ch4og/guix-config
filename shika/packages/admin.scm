9e6f115e29478b4b1e28bedfb5825e76ca5c810d
Nikita Mitasov <me@ch4og.com>
2026-08-18 11:35:09 +0300

    shika: Add btop-nvidia.
    * shika/packages/admin.scm (btop-nvidia): New variable.

--- shika/packages/admin.scm
+++ shika/packages/admin.scm
@@ -2,8 +2,11 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (shika packages admin)
+  #:use-module (guix gexp)
   #:use-module (guix packages)
+  #:use-module (guix utils)
   #:use-module (gnu packages admin)
+  #:use-module (gnu packages gl)
   #:use-module (shika utils override))
 
 (define-public fastfetch-no-zfs
@@ -12,3 +15,19 @@
                   #:commit (package-version fastfetch-minimal)
                   #:inputs (modify-inputs (package-inputs fastfetch)
                              (delete "zfs"))))
+
+(define-public btop-nvidia
+  (package
+    (inherit btop)
+    (name "btop-nvidia")
+    (inputs (cons mesa
+                  (package-inputs btop)))
+    (arguments
+     (substitute-keyword-arguments (package-arguments btop)
+       ((#:make-flags flags #~(list))
+        #~(cons (string-append "LDFLAGS+=-Wl,-rpath="
+                               #$(this-package-input "mesa") "/lib")
+                #$flags))))
+    (description
+     (string-append (package-description btop)
+                    " NVIDIA GPU support requires using replace-mesa."))))
