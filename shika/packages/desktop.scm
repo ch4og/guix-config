2a9459e37604f70a7dd62bfb588039c31ab18b90
Nikita Mitasov <me@ch4og.com>
2026-08-24 19:54:27 +0300

    shika: Move hyperheadset to (shika packages desktop).
    * shika/packages/desktop.scm (hyperheadset, hyperheadset-udev-rules):
    Move definitions here.
    * shika/packages/hyperheadset.scm (hyperheadset, hyperheadset-udev-rules):
    Add deprecated aliases.

--- shika/packages/desktop.scm
+++ shika/packages/desktop.scm
@@ -2,16 +2,20 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (shika packages desktop)
+  #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (guix git-download)
+  #:use-module (guix build-system cargo)
   #:use-module (guix build-system copy)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gnome)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages suckless)
+  #:use-module (shika utils cargo)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public dmenu-bluetooth
@@ -76,3 +80,50 @@
     (description
      "Manage NetworkManager connections with supported launchers instead of nm-applet.")
     (license license:expat)))
+
+(define-public hyperheadset
+  (package
+    (name "hyperheadset")
+    (version "1.10.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/LennardKittner/HyperHeadset")
+              (commit (string-append "v" version))
+              (recursive? #t)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0y3v6qfppa5qz7aa7y6swpm9r8xc56v8ldv8y3zw8h3hzbsm2x98"))))
+    (build-system cargo-build-system)
+    (arguments
+     (list
+      #:install-source? #f
+      #:features ''("eq-editor")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'install 'install-extras
+            (lambda* _
+              (let ((share (string-append #$output "/share")))
+                (mkdir-p (string-append share "/applications"))
+                (copy-file "hyper-headset.desktop"
+                           (string-append share "/applications/hyper-headset.desktop"))))))))
+    (native-inputs (list pkg-config))
+    (inputs (cons* dbus
+                   eudev
+                   (shika-cargo-inputs 'hyperheadset)))
+    (home-page "https://github.com/LennardKittner/HyperHeadset")
+    (synopsis "Application for monitoring and managing HyperX headsets.")
+    (description
+     "A CLI and tray application for monitoring and managing HyperX headsets.")
+    (license license:expat)))
+
+(define-public hyperheadset-udev-rules
+  (package
+    (inherit hyperheadset)
+    (name "hyperheadset-udev-rules")
+    (build-system copy-build-system)
+    (arguments
+     `(#:install-plan '(("99-HyperHeadset.rules" "lib/udev/rules.d/"))))
+    (synopsis "UDev rules for HyperHeadset")
+    (description "UDev rules granting permissions for HyperX headsets.")))
